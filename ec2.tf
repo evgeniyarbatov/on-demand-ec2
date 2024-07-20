b40992e24f7c6d54e3632d557dde4eb54e356a56
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-20 15:09:51 +0700

    nit

--- ec2.tf
+++ ec2.tf
@@ -84,7 +84,6 @@ resource "aws_instance" "ec2" {
       "sudo curl -SL https://github.com/docker/compose/releases/download/v2.28.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose",
-      "sudo service docker start",
       "sudo cp osrm.service /etc/systemd/system/osrm.service",
       "sudo systemctl daemon-reload",
       "sudo systemctl enable osrm",
