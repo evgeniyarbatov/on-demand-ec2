b94a2d13dc81968920c7048e460fd70561c26f7b
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-21 15:53:47 +0700

    I need public IP for my EC2

--- ec2.tf
+++ ec2.tf
@@ -56,11 +56,12 @@ resource "aws_security_group" "ec2-sec-gr" {
 }
 
 resource "aws_instance" "ec2" {
-  ami                    = data.aws_ami.linux.id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  subnet_id              = aws_subnet.osrm_subnet.id
-  vpc_security_group_ids = [aws_security_group.ec2-sec-gr.id]
+  ami                         = data.aws_ami.linux.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = aws_subnet.osrm_subnet.id
+  vpc_security_group_ids      = [aws_security_group.ec2-sec-gr.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "osrm"
