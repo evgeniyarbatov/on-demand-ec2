c843eb5b095b93dc3bba96718cadc763486c9ef6
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-22 13:55:39 +0700

    Success calling EC2 OSRM

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,7 +1,6 @@
 import urllib3
 import boto3
 import os
-import json
 
 def lambda_handler(event, context):
     instance_id = os.environ['INSTANCE_ID']
@@ -11,12 +10,9 @@ def lambda_handler(event, context):
     overview = event["queryStringParameters"].get("overview")
     
     ec2 = boto3.client('ec2')
-    response = ec2.describe_instances(InstanceIds=[instance_id])
-    print(response)
     
+    response = ec2.describe_instances(InstanceIds=[instance_id])    
     public_dns = response['Reservations'][0]['Instances'][0]['PublicDnsName']
-    
-    print(f"http://{public_dns}:5000/route/v1/foot/{points}")
     
     http = urllib3.PoolManager()
     url = f"http://{public_dns}:5000/route/v1/foot/{points}"
@@ -25,11 +21,8 @@ def lambda_handler(event, context):
         'overview': overview,
     }
     encoded_params = urllib3.request.urlencode(params)
-
-    response = http.request('GET', f"{url}?{encoded_params}")
-    result = json.loads(response.data.decode('utf-8'))
-    
-    # ec2.stop_instances(InstanceIds=[instance_id])
+    response = http.request('GET', f"{url}?{encoded_params}")    
+    result = response.data.decode('UTF-8')
 
     return {
         'statusCode': 200, 
