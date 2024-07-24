7e8421bd816f48ed085d155ea6b6d84f7e94b296
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-24 22:45:45 +0800

    Do not return body

--- lambda/start_ec2.py
+++ lambda/start_ec2.py
@@ -8,12 +8,9 @@ def lambda_handler(event, context):
     instance = ec2.describe_instances(InstanceIds=[instance_id])
     
     instance_state = instance['Reservations'][0]['Instances'][0]['State']['Name']
-    if instance_state == 'stopped':
+    if instance_state != 'running':
       ec2.start_instances(InstanceIds=[instance_id])
 
     return {
-      'statusCode': 200,
-      'body': {
-        'success': 'true',
-      }
+      'statusCode': 200
     }
