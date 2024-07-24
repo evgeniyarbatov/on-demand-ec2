194d22c92e327ad08f212323e0acb85a99080ba6
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-24 22:43:06 +0800

    Rollback

--- lambda/start_ec2.py
+++ lambda/start_ec2.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     instance = ec2.describe_instances(InstanceIds=[instance_id])
     
     instance_state = instance['Reservations'][0]['Instances'][0]['State']['Name']
-    if instance_state != 'running':
+    if instance_state == 'stopped':
       ec2.start_instances(InstanceIds=[instance_id])
 
     return {
