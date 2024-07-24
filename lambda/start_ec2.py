aee6aeb304fc48a14b643bde5e65c4f28b5240b4
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-24 22:41:05 +0800

    Do not throw error if EC2 is already running

--- lambda/start_ec2.py
+++ lambda/start_ec2.py
@@ -5,7 +5,11 @@ def lambda_handler(event, context):
     instance_id = os.environ['INSTANCE_ID']
     
     ec2 = boto3.client('ec2')
-    ec2.start_instances(InstanceIds=[instance_id])
+    instance = ec2.describe_instances(InstanceIds=[instance_id])
+    
+    instance_state = instance['Reservations'][0]['Instances'][0]['State']['Name']
+    if instance_state != 'running':
+      ec2.start_instances(InstanceIds=[instance_id])
 
     return {
       'statusCode': 200,
