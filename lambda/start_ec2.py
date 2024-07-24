b383d49f224ab21a241768576469c2d21f4d1c38
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-24 22:36:05 +0800

    Nits

--- lambda/start_ec2.py
+++ lambda/start_ec2.py
@@ -8,5 +8,8 @@ def lambda_handler(event, context):
     ec2.start_instances(InstanceIds=[instance_id])
 
     return {
-      'statusCode': 200
+      'statusCode': 200,
+      'body': {
+        'success': 'true',
+      }
     }
