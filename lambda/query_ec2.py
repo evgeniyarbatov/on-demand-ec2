2992886d879290e9b33bc16a4cf399965f574cc5
Evgeny Arbatov <arbatovevgeny@gmail.com>
2025-12-18 11:36:35 +0700

    chore: format python code with black

--- lambda/query_ec2.py
+++ lambda/query_ec2.py
@@ -2,32 +2,33 @@ import urllib3
 import boto3
 import os
 
+
 def lambda_handler(event, context):
-    instance_id = os.environ['INSTANCE_ID']
-    
+    instance_id = os.environ["INSTANCE_ID"]
+
     points = event["pathParameters"].get("points")
     geometries = event["queryStringParameters"].get("geometries")
     overview = event["queryStringParameters"].get("overview")
-    
-    ec2 = boto3.client('ec2')
 
-    waiter = ec2.get_waiter('instance_running')
+    ec2 = boto3.client("ec2")
+
+    waiter = ec2.get_waiter("instance_running")
     waiter.wait(InstanceIds=[instance_id])
-    
-    response = ec2.describe_instances(InstanceIds=[instance_id])    
-    public_dns = response['Reservations'][0]['Instances'][0]['PublicDnsName']
-    
+
+    response = ec2.describe_instances(InstanceIds=[instance_id])
+    public_dns = response["Reservations"][0]["Instances"][0]["PublicDnsName"]
+
     http = urllib3.PoolManager()
     url = f"http://{public_dns}:5000/route/v1/foot/{points}"
     params = {
-        'geometries': geometries,
-        'overview': overview,
+        "geometries": geometries,
+        "overview": overview,
     }
     encoded_params = urllib3.request.urlencode(params)
-    response = http.request('GET', f"{url}?{encoded_params}")    
-    result = response.data.decode('UTF-8')
+    response = http.request("GET", f"{url}?{encoded_params}")
+    result = response.data.decode("UTF-8")
 
     return {
-        'statusCode': 200, 
-        'body': result,
+        "statusCode": 200,
+        "body": result,
     }
--- lambda/start_ec2.py
+++ lambda/start_ec2.py
@@ -1,16 +1,15 @@
 import boto3
 import os
 
-def lambda_handler(event, context):
-    instance_id = os.environ['INSTANCE_ID']
-    
-    ec2 = boto3.client('ec2')
-    instance = ec2.describe_instances(InstanceIds=[instance_id])
-    
-    instance_state = instance['Reservations'][0]['Instances'][0]['State']['Name']
-    if instance_state != 'running':
-      ec2.start_instances(InstanceIds=[instance_id])
 
-    return {
-      'statusCode': 200
-    }
+def lambda_handler(event, context):
+    instance_id = os.environ["INSTANCE_ID"]
+
+    ec2 = boto3.client("ec2")
+    instance = ec2.describe_instances(InstanceIds=[instance_id])
+
+    instance_state = instance["Reservations"][0]["Instances"][0]["State"]["Name"]
+    if instance_state != "running":
+        ec2.start_instances(InstanceIds=[instance_id])
+
+    return {"statusCode": 200}
